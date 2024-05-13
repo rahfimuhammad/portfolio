c3fb0b20c883e530608ca747e0b6da5a663aa86f
rahfimuhammad <rahfi.muhammad@alumni.ui.ac.id>
2024-05-14 02:21:45 +0700

    font family

--- index.js
+++ index.js
@@ -2,27 +2,27 @@ const cardContainer = document.querySelector('.card-container');
 
 const projectsData =[
     {
-        name: 'Website Interaktif Arkeologi',
+        name: 'Arkeologi Hari Ini',
         link: 'https://rahfimuhammad.github.io/arkeologi-hari-ini/',
         image: './assets/arkeologihariini.png',
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quisquam explicabo totam? Dolore iusto earum, quo odio recusandae quibusdam ipsum itaque dolorum aut neque, architecto cum fugit pariatur qui et!"
     },
     {
-        name: 'KATA (aplikasi chat realtime)',
+        name: 'allyouneedistypescript',
+        link: 'https://allyouneedistypescript.vercel.app',
+        image: './assets/allyouneedistypescript.png',
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quisquam explicabo totam? Dolore iusto earum, quo odio recusandae quibusdam ipsum itaque dolorum aut neque, architecto cum fugit pariatur qui et!"
+    },
+    {
+        name: 'KATA',
         link: 'https://kata-app.netlify.app',
         image: './assets/kata.png',
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quisquam explicabo totam? Dolore iusto earum, quo odio recusandae quibusdam ipsum itaque dolorum aut neque, architecto cum fugit pariatur qui et!"
     },
     {
-        name: 'SeblakGoInter (aplikasi pemesanan dan analitik restaurant)',
+        name: 'SeblakGoInter',
         link: 'https://seblakgointer.netlify.app',
         image: './assets/seblakGoInter.png',
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quisquam explicabo totam? Dolore iusto earum, quo odio recusandae quibusdam ipsum itaque dolorum aut neque, architecto cum fugit pariatur qui et!"
-    },
-    {
-        name: 'allyouneedistypescript (aplikasi task manager, kalkulator, dan kamus)',
-        link: 'https://allyouneedistypescript.vercel.app',
-        image: './assets/allyouneedistypescript.png',
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quisquam explicabo totam? Dolore iusto earum, quo odio recusandae quibusdam ipsum itaque dolorum aut neque, architecto cum fugit pariatur qui et!"
     }
 ]
@@ -32,9 +32,12 @@ projectsData.map(item => {
     card.classList.add('card');
     
     card.innerHTML = `
-        <div class="card-description">
-            <h2><a href="${item.link}">${item.name}</a></h2>
+        <div class="card-content">
+            <h2>${item.name}</h2>
             <p>${item.description}</p>
+            <div class="card-button">
+                <a href="${item.link}">View Project</a>
+            </div>
         </div>
         <img src="${item.image}" alt="${item.name}">`;
 
